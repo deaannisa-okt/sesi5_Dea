ae9f4cc1a02837b54630ab9143732993b9da94e6
Ilham <deaannisa.okt@gmail.com>
2025-08-02 20:11:42 +0700

    add file

---
+++ Sesi3_Dea.js
@@ -0,0 +1,11 @@
+// console.log("Halo");
+
+let tinggi = 10;
+
+for (let i = 1; i <= tinggi; i++) {
+  let baris = "";
+  for (let j = 1; j <= i; j++) {
+    baris += "*";
+  }
+  console.log(baris);
+}
